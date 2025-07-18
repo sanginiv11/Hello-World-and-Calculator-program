12b649200a54ab534aa7fdb7511364c9ef9513a3
Sangini Verma <sanginiv11@gmail.com>
2025-07-18 22:22:16 +0530

    Add files via upload

---
+++ calcprg.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, sum=0;
+    cout<<"Enter first number: ";
+    cin>>num1;
+    cout<<"Enter second number: ";
+    cin>>num2;
+
+    sum=num1+num2;
+    cout<<"Sum of given numbers = "<<sum;
+
+    return 0;
+}
